e72f5e75b91ce9183c78437d958d24a1eaef5d07
DartVlayder <mr.6990@mail.ru>
2025-08-21 01:00:14 +0300

    FSM Машина состояний, Админка в боте

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.strategy import FSMStrategy
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
+from handlers.admin_private import admin_router
 
 from common.bot_cmds_list import private
 
@@ -19,10 +21,13 @@ bot = Bot(
     token=os.getenv("TOKEN"),
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
+bot.my_admins_list = []
+
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
+dp.include_router(admin_router)
 
 
 async def main():
