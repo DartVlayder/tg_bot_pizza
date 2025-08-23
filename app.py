56950922b670d7bff9dd5006cb2b8c1abef1ad8f
DartVlayder <mr.6990@mail.ru>
2025-08-23 20:38:24 +0300

    Продолжение работы с базой данных

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from aiogram.fsm.strategy import FSMStrategy
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
-
+from database.engine import create_db, drop_db
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
@@ -29,8 +29,24 @@ dp.include_router(user_private_router)
 dp.include_router(user_group_router)
 dp.include_router(admin_router)
 
+async def on_startup(bot):
+
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+
+async def on_shutdown(bot):
+    print('бот лег')
+
 
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    await create_db()
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
